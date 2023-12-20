a6c62cd6a8d633d8678c31a4b330cbc9c7d2e903
vathsm <samanthavath@gmail.com>
2023-12-19 21:06:46 -0800

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("test333")
+todos = []
+
+while True:
+    user_action = input("Type add, show, edit, or exit:\n")
+    user_action = user_action.strip()
+
+    match user_action:
+        case "add":
+            todo = input("Enter a To Do:\n")
+            todos.append(todo)
+        case "show":
+            for item in todos:
+                print(item)
+        case "edit":
+            number = int(input("Number of the To Do to edit:\n"))
+            number = number - 1
+            new_todo = input("Enter new To Do:\n")
+            todos[number] = new_todo
+        case "exit":
+            break

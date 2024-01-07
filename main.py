7bcd431f192c719ddd45e98861ba2687cf8cb687
vathsm <samanthavath@gmail.com>
2024-01-06 21:14:16 -0800

    app day 5 update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit, or exit:\n")
+    user_action = input("Type add, show, edit, completed or exit:\n")
     user_action = user_action.strip()
 
     match user_action:
@@ -9,15 +9,16 @@ while True:
             todo = input("Enter a To Do:\n")
             todos.append(todo)
         case "show":
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(f"{index + 1}. {item}")
         case "edit":
-            number = int(input("Number of the To Do to edit:\n"))
-            number = number - 1
+            number = int(input("Number of the To Do to edit:\n")) - 1
             new_todo = input("Enter new To Do:\n")
             todos[number] = new_todo
+        case "completed":
+            user_input = int(input("Enter completed item number:\n")) - 1
+            todos.pop(user_input)
         case "exit":
             break
 
 print("Thank You!")
-print("this is a test for git (can remove)")

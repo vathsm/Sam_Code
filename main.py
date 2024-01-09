31971af97f04b03624a60438ba8d03869575d749
vathsm <samanthavath@gmail.com>
2024-01-08 20:13:16 -0800

    txt file

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-todos = []
 
 while True:
-    user_action = input("Type add, show, edit, completed or exit:\n")
+    user_action = input("Type add, show, edit, completed or exit:")
     user_action = user_action.strip()
 
     match user_action:
         case "add":
-            todo = input("Enter a To Do:\n")
+            todo = input("Enter a To Do:") + "\n"
+
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open("todos.txt", "w")
+            file.writelines(todos)
+            file.close()
         case "show":
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 print(f"{index + 1}. {item}")
+
         case "edit":
-            number = int(input("Number of the To Do to edit:\n")) - 1
-            new_todo = input("Enter new To Do:\n")
+            number = int(input("Number of the To Do to edit:")) - 1
+            new_todo = input("Enter new To Do:")
             todos[number] = new_todo
+
         case "completed":
-            user_input = int(input("Enter completed item number:\n")) - 1
+            user_input = int(input("Enter completed item number:")) - 1
             todos.pop(user_input)
+
         case "exit":
             break
 
 print("Thank You!")
-print("This is Sam's branch")
